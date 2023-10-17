7870c65f6bc8880a20ae7f54488380e696a15a24
ElDEEB21 <116762512+ElDEEB21@users.noreply.github.com>
2023-10-18 00:56:14 +0300

    Update main.h

--- main.h
+++ main.h
@@ -3,7 +3,18 @@
 
 #include <stdlib.h>
 #include <stdarg.h>
+typedef struct flags
+{
+	int plus;
+	int space;
+	int hash;
+} flags_t;
 
+typedef struct printHandler
+{
+	char c;
+	int (*f)(va_list ap, flags_t *f);
+} ph;
 int print_int(va_list l, flags_t *f);
 void print_number(int n);
 int print_unsigned(va_list l, flags_t *f);
