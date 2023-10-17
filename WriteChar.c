22a0b3d429c73eeca53944fa94db3c671e447ab4
ElDEEB21 <116762512+ElDEEB21@users.noreply.github.com>
2023-10-18 00:14:42 +0300

    Update WriteChar.c

--- WriteChar.c
+++ WriteChar.c
@@ -2,7 +2,7 @@
 #include <unistd.h>
 /**
  * _putchar - writes the character c to stdout
- * @c: The character will print
+ * @ch: The character will print
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  * Description: _putchar uses a local buffer of 1024 to call write
