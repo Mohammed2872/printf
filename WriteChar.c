834e29ed2d0c05132fb39b1e8a6b0e0d0e7f2749
ElDEEB21 <116762512+ElDEEB21@users.noreply.github.com>
2023-10-18 00:53:21 +0300

    Update WriteChar.c

--- WriteChar.c
+++ WriteChar.c
@@ -3,39 +3,40 @@
 
 /**
  * _putchar - writes the character c to stdout
- * @ch: The character will print
+ * @c: The character to print
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  * Description: _putchar uses a local buffer of 1024 to call write
  * as little as possible
  */
-int _putchar(char ch)
+int _putchar(char c)
 {
 	static char buf[1024];
 	static int i;
 
-	if (ch == -1 || i >= 1024)
+	if (c == -1 || i >= 1024)
 	{
 		write(1, &buf, i);
 		i = 0;
 	}
-	if (ch != -1)
+	if (c != -1)
 	{
-		buf[i] = ch;
+		buf[i] = c;
 		i++;
 	}
 	return (1);
 }
+
 /**
- * _puts -> prints a string to stdout
+ * _puts - prints a string to stdout
  * @str: pointer to the string to print
- * Return: num of chars has written
+ * Return: number of chars written
  */
-int _puts(char *s)
+int _puts(char *str)
 {
 	register int i;
 
-	for (i = 0; s[i] != '\0'; i++)
-		_putchar(s[i]);
+	for (i = 0; str[i] != '\0'; i++)
+		_putchar(str[i]);
 	return (i);
 }
